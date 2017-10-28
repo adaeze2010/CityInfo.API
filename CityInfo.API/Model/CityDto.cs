aefb9e87014df8b5e470f88c5a5ce5e6a667cc6a
Adaeze Onuekwusi <Adaeze.Onuekwusi@fourth.com>
2017-10-28 14:35:54 +0100

    Add project files.

---
+++ CityInfo.API/Model/CityDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Model
+{
+    public class CityDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int NumberOfPointsOfInterest { get
+            {
+                return PointsInterest.Count;
+
+            } }
+
+
+        public ICollection<PointOfInterestDto> PointsInterest { get; set; }
+        = new List<PointOfInterestDto>();
+    }
+}
---
+++ CityInfo.API/Startup.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+
+
+namespace CityInfo.API
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+               .AddMvcOptions(o => o.OutputFormatters.Add(
+                  new XmlDataContractSerializerOutputFormatter()));
+            //{
+            //    if (o.SerializerSettings.ContractResolver != null)
+            //    {
+            //        var castedResolver = o.SerializerSettings.ContractResolver
+            //        as DefaultContractResolver;
+            //        castedResolver.NamingStrategy = null;
+            //    }
+            //}
+            //    );
+
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerfactory)
+        {
+            loggerfactory.AddConsole();
+            loggerfactory.AddDebug( );
+
+            loggerfactory.AddNLog();
+
+            //app.UseWelcomePage();
+           
+           
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+           else
+            {
+
+                app.UseExceptionHandler();
+            }
+            app.UseStatusCodePages();
+            app.UseMvc();
+           
+        }
+    }
+}
